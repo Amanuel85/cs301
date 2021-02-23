88da689a094084b96a86ac98fad7169d2ee2f7ac
Amanuel85 <40584879+Avunue@users.noreply.github.com>
2021-02-23 10:52:27 -0600

    install prompt-sync

---
+++ w2d2datatype/w2d2Assignment.js
@@ -0,0 +1,9 @@
+"use strict";
+// 1 
+let prompt = require("prompt-sync")();
+let quarts = prompt("enter volume in quarts");
+let litter  =  quarts * 0.946
+console.log(litter)
+// 2 
+let distKm = prompt("What is the distance in Km ?")
+let distMile = 
