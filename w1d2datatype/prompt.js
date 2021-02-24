b37be6108e5a52f730637486ec3d6e854f79ae31
Amanuel85 <40584879+Avunue@users.noreply.github.com>
2021-02-23 21:24:13 -0600

    update assignment

--- w1d2datatype/prompt.js
+++ w1d2datatype/prompt.js
@@ -1,4 +1,38 @@
 "use strict";
 const prompt = require("prompt-sync")();
-let name = prompt("What is your name?:");
-console.log(`Hi ${name}`);
+// let name = prompt("What is your name?:");
+// console.log(`Hi ${name}`);
+// console.log(true * "7");
+
+//let prompt = require('prompt-sync')();
+// let tempInCelsius = prompt("Eneter value in celsius: ");
+// let tempInFahrenheit = (9 / 5) * parseFloat(tempInCelsius) + 32;
+// console.log(tempInFahrenheit);
+
+// let tempInFahrenheit = prompt("Enter value in Fahrenheit? :");
+// let tempInCelsius = (Number(tempInFahrenheit - 32) * 5) / 9;
+// console.log(tempInCelsius);
+
+// const foo = 5;
+// const bat = "happy day";
+// const zoo = foo && bat;
+// console.log(zoo);
+let radius = prompt("Enter the radius");
+let height = prompt("Enter height");
+radius = Number(radius);
+height = Number(height);
+let Volume = Math.PI * radius * radius * height;
+console.log(Volume);
+//exercise 2
+let x1 = prompt("Enter x1");
+let x2 = prompt("Enter x2");
+let y1 = prompt("Enter y1");
+let y2 = prompt("Enter y2");
+const xdif = x2 - x1;
+const ydif = y2 - y1;
+let sumsqr = xdif * xdif + ydif * ydif;
+let d = Math.sqrt(sumsqr);
+console.log(d);
+
+//let d = Math.sqrt((x2 - x1) ** 2) * (y2 - y1) ** 2;
+//console.log(d);
--- w1d2datatype/w2d2assignment.js
+++ w1d2datatype/w2d2assignment.js
@@ -1,9 +1,49 @@
 "use strict";
-// 1
 let prompt = require("prompt-sync")();
-let quarts = prompt("enter volume in quarts");
+
+// Question 1
+
+let quarts = prompt("What is the  volume in quarts ?:");
+quarts = Number(quarts);
 let litter = quarts * 0.946;
-console.log(litter);
-// 2
-let distKm = prompt("What is the distance in Km ?");
-//let distMile =
+console.log(`The volume in litter is ${litter}`);
+
+// Question  2
+let distKm = prompt("What is the distance in Km ?:");
+distKm = Number(distKm);
+let distMile = distKm * 0.62;
+console.log(`The distance in mile is ${distMile}`);
+
+//Question 3
+
+const numberOfboxes = prompt("Enter total number of boxes: ");
+const boxesPerstack = prompt("Enter number of boxes in each stack: ");
+let numberofStack = numberOfboxes / boxesPerstack;
+let remainder = numberOfboxes % boxesPerstack;
+if (remainder == 0) {
+  numberofStack = numberOfboxes / boxesPerstack;
+} else {
+  numberofStack = parseInt(numberofStack) + 1;
+}
+console.log(
+  `For ${numberOfboxes} boxes if you put ${boxesPerstack} boxes per stack you need ${numberofStack} stacks`
+);
+
+// Question 4
+
+const begnningOdometer = prompt("Enter beginning odometer reading: ");
+const endingOdometer = prompt("Enter ending odometer reading: ");
+const galonsOfgas = prompt("Enter the total number of galons of gas: ");
+const totalMilage = endingOdometer - begnningOdometer;
+const milagePergalon = Math.ceil(totalMilage / galonsOfgas);
+
+console.log(`Your beginning mileage was ${begnningOdometer}, your ending mileage was ${endingOdometer} and 
+ you used ${galonsOfgas} galons of gas, so your milage per gallon is ${milagePergalon} miles/galon`);
+
+//Question 5
+const age = prompt("What is your age?: ");
+const maxHeartrate = 220 - age;
+const slowestRate = (maxHeartrate * 65) / 100;
+const fastestRate = (maxHeartrate * 85) / 100;
+console.log(`For your age your maximum heart rate is ${maxHeartrate},
+ your slowest heart rate is :${slowestRate} and your fastest  heart rate is:${fastestRate} per minute`);
