aa6d54fe14f74b750fd63dc5a24f9a1495d6d1d9
Amanuel85 <40584879+Avunue@users.noreply.github.com>
2021-02-26 19:15:13 -0600

    function update

--- w1d5functions/functionpractice.js
+++ w1d5functions/functionpractice.js
@@ -11,10 +11,76 @@ Write a function named testPrime that returns true when
 argument to the function is a prime number, otherwise returns false.
 • Now call the function to test if user input is prime or not. */
 
-function testPrime(num){
-    let isprime = true;
-    for(let i = 2;i<num;i++){
-        if(num%i=== 0){
-            isprime = false}}
-    return isprime}
-console.log(testPrime(15))
+// function testPrime(num){
+//     let isprime = true;
+//     for(let i = 2;i<num;i++){
+//         if(num%i=== 0){
+//             isprime = false}}
+//     return isprime}
+// console.log(testPrime(15))
+// let userName = "John";
+// function showMessage() {
+// let userName = "Bob"; // declare a local variable
+// let message = 'Hello, ' + userName; // Bob
+// console.log(message);
+// }
+// showMessage();
+// console.log( userName ); // John, unchanged
+
+// function a(){
+//     console.log(x); // consult Global for x and print 20 from Global
+//     }
+//     function b(){
+//     let x = 10;
+//     a(); // consult Global for a
+//     console.log(x);
+//     }
+//     let x = 20;
+//     b();
+// function f() {
+//     let a = 1, b = 20, c;
+//     console.log(a + " " + b + " " + c); // 1 20 undefined
+//     function g() {
+//     let b = 300, c = 4000;
+//     console.log(a + " " + b + " " + c); // 1 300 4000
+//     a = a + b + c;
+//     console.log(a + " " + b + " " + c); // 4301 300 4000
+//     }
+//     console.log(a + " " + b + " " + c); // 1 20 undefined
+//     g();
+//     console.log(a + " " + b + " " + c); // 4301 20 undefined
+//     }
+//     f();
+
+// let x = 10;
+// function main() {
+// let x = 0;
+// console.log("x1 is " + x);
+// x = 20;
+// console.log("x2 is " + x);
+// if (x > 0) {
+// x = 30;
+// console.log("x3 is " + x);
+// }
+// console.log("x4 is " + x);
+// function f(x) {
+// console.log("x5 is " + x);
+// }
+// f(50);
+// console.log("x6 is " + x);
+// console.log(x)}
+// main();
+// console.log("x7 is " + x);
+
+// let num = 8;
+// let sum = 0;
+// for(let i = 1;i<num;i++){
+//     if(num %i === 0){
+//         sum = sum+i
+//     }
+// }
+// console.log(sum)
+// if (num === sum){
+//     console.log(num + "is perfect number")
+//     }
+//     else {console.log("it is not perfect number")}
---
+++ w1d5functions/w1d5function.js
@@ -0,0 +1,96 @@
+"use strict"
+let prompt = require("prompt-sync")();
+
+
+/*
+1. checkPrime
+a. Write a function named checkPrime that accepts a parameter and returns true if the
+argument is a prime number otherwise returns false.
+b. Now write a program that prompts user to input a number and calls the function
+checkPrime to see if the entered number is a prime number or not.
+input : number;
+process:  start for loop from 2 to one less the number 
+if number divided by i === 0 then it is not prime numbe
+*/
+//a
+
+
+function isPrime(n){
+    let prime = true;
+         for(let i = 2;i<n;i++){
+             if(n%i===0){
+             prime =  false}
+         }
+return prime;}
+console.log(isPrime(11));
+//b
+let number = +prompt("Enter a number: ")
+console.log(isPrime(number))
+
+/* question 2 volumeOfCylinder
+a. Write a function, areaOfCircle, that computes and returns area of a circle based on the
+value of input radius.
+b. Write a function, volumeOfCylinder, that compute volume of a cylinder and making use
+of the areaOfCircle function.
+c. Write code to call and test your function
+input 
+*/
+//a
+
+ function areaOfCircle(radius){
+    let circleArea = Math.floor(Math.PI*(radius*radius))
+    
+    return circleArea
+    //console.log(circleArea)
+}
+console.log(areaOfCircle(5))
+
+//b  
+function volumeOfCylinder(radius, height){
+    let cylnderVolume = Math.floor((areaOfCircle(radius)* height))
+    //console.log(cylnderVolume)
+    return cylnderVolume
+}
+//volumeOfCylinder(5,2)
+console.log(volumeOfCylinder(5,2))
+// c
+let radius = +prompt("Enter radius")
+let height = +prompt("Enter height")
+console.log("The radius of the circle is:",areaOfCircle(radius))
+console.log("The volume of the cylnder is: ", volumeOfCylinder(radius,height))
+
+/*  Q3
+ * Write a program that calculates the total volume of a house, including the volume of the roof
+space. Your program must allow the user to enter width, depth, height and sweep as shown in
+figure below. Write code to call and test your function
+ */
+let houseWidth = +prompt("Enter the width of the house: ");
+let houseDepth = +prompt("Enter the depth  of the house: ");
+let houseHeight = +prompt("Enter the height  of the house: ");
+let houseSweep = +prompt("Enter the Sweep  of the house: ");
+
+function livingvolume(houseWidth,houseHeight,houseDepth){
+    let livingAreaVolume = houseWidth*houseHeight*houseDepth
+    return livingAreaVolume
+}
+function triangleArea(a = houseSweep,b = houseSweep,c = houseWidth){
+    let s = (a+b+c)/2
+    let areaofTriangle = Math.sqrt((s-a)*(s-b)*(s-c))
+    
+   areaofTriangle = Math.floor(areaofTriangle)
+   return areaofTriangle
+}
+
+//console.log(triangleArea())
+function roofVolume(trianglearea,houseDepth){
+    let volumeofRoof = houseSweep *  triangleArea()  //roof volume = treagle are *
+    return volumeofRoof
+}
+function houseVolume(living,roof){ // house volume = living room volume = roof voluem 
+    return living + roof
+}
+console.log("The house voulume is "
+,houseVolume(livingvolume(houseWidth,houseHeight,houseDepth),roofVolume(houseSweep,triangleArea())))
+
+
+//console.log("The volume of the house is: ",)
